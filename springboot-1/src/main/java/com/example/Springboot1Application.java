01168f29a7e7b29f0c5dc4a9ca1837804050e837
ChaoActor <chao_actor@163.com>
2019-01-22 22:48:53 +0800

    SpringBoot-删除多余的配置

--- springboot-1/src/main/java/com/example/Springboot1Application.java
+++ springboot-1/src/main/java/com/example/Springboot1Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy(proxyTargetClass = true,exposeProxy = true)
 @SpringBootApplication
 public class Springboot1Application {
 
--- springboot-1/src/main/java/com/example/controller/HelloController.java
+++ springboot-1/src/main/java/com/example/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import com.example.service.HelloService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
