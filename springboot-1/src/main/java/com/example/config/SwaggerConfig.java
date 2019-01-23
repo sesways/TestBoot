25fd314e30012e0b3038f5fb7f02f933420ac773
ChaoActor <chao_actor@163.com>
2019-01-23 23:48:00 +0800

    SpringBoot-完善注释

--- springboot-1/src/main/java/com/example/config/SwaggerConfig.java
+++ springboot-1/src/main/java/com/example/config/SwaggerConfig.java
@@ -2,22 +2,21 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
+/**
+ * Swagger2的配置类；
+ */
 @Configuration
 public class SwaggerConfig {
+    private List<VendorExtension> list = new ArrayList<>();
 
     @Bean
     public Docket docket(){
@@ -30,121 +29,6 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo(){
         return new ApiInfo("My Swagger2", "这个参数是描述", "1.0", "http://www.baidu.com",
-                ApiInfo.DEFAULT_CONTACT, "", "", new List<VendorExtension>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<VendorExtension> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(VendorExtension vendorExtension) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends VendorExtension> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends VendorExtension> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public VendorExtension get(int index) {
-                return null;
-            }
-
-            @Override
-            public VendorExtension set(int index, VendorExtension element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, VendorExtension element) {
-
-            }
-
-            @Override
-            public VendorExtension remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<VendorExtension> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<VendorExtension> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<VendorExtension> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        });
-    };
+                ApiInfo.DEFAULT_CONTACT, "", "", list);
+    }
 }
